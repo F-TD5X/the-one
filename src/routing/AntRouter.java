9aafaea52f5bf4478c5064553ccfa772e76a0c60
F_TD5X <mjikop1231@gmail.com>
2019-04-29 15:35:30 +0800

    v0.2.1 release

--- src/routing/AntRouter.java
+++ src/routing/AntRouter.java
@@ -1,252 +1,228 @@
+/**
+ * Author: F_TD5X
+ */
+
 package routing;
 
 import core.*;
 import routing.util.RoutingInfo;
 import util.Tuple;
 
+import java.lang.reflect.AnnotatedType;
 import java.util.*;
 
-public class AntRouter extends ActiveRouter {
+public class AntRouter extends ActiveRouter{
 
-    private static final String ANT_NS = "AntRouter";
+    /** Ant Router's setting namespace ({@value})*/
+    private static String ANT_NS = "AntRouter";
 
-    private static double UPDATE_INTERVAL;               // Update Interval
-    private static double PRED_INC;                      // prediction increased value per times.
-    private static double PRED_DEC;                      // prediction decreased value pre times.
-    private static double PRED_DEC_RATE;                 // prediction decreasing rate for hops.
+    /** update interval */
+    private static double UPDATE_INTERVAL;
+    private static double DEFAULT_UPDATE_INTERVAL = 1000;
 
-    private Map<DTNHost, Double> predictions;            // Ant Router predictions table.
-    private Map<DTNHost, Double> predictions_update;     // predictions update time.
-    private List<DTNHost> known_host;                   // Discovered Host List.
-    private double last_update_time;                    // lase update time.
+    /** increase value of predictions */
+    private static double PREDS_INC;
+    private static double DEFAULT_PREDS_INC = 10;
 
 
-    /**
-     * Constructor. Create Message Router based on settings in Settings Object.
-     *
-     * @param s Settings Object
-     */
-    public AntRouter(Settings s) {
+    /** decrease rate of predictions */
+    private static double PREDS_DEC;
+    private static double DEFAULT_PREDS_DEC = 0.5;
+
+    private static int ANTS_PRE_MSG;
+    private static int DEFAULT_ANTS_PRE_MSG = 50;
+
+
+    /** delivery predictabilities */
+    private Map<DTNHost,Double> predictions;
+
+    private Map<DTNHost,Double> update_time;
+
+    public AntRouter(Settings s){
         super(s);
-        Settings ant_router_settings = new Settings(ANT_NS);
+        Settings antRouterSettings = new Settings(ANT_NS);
 
-        predictions = new HashMap<>();
-        predictions_update = new HashMap<>();
-        known_host = new ArrayList<>();
+        if(antRouterSettings.contains("UPDATE_INTERVAL"))
+            UPDATE_INTERVAL = antRouterSettings.getDouble("UPDATE_INTERVAL");
+        else
+            UPDATE_INTERVAL = DEFAULT_UPDATE_INTERVAL;
 
-        UPDATE_INTERVAL = ant_router_settings.getDouble("UPDATE_INTERVAL");
+        if(antRouterSettings.contains("PREDS_INC"))
+            PREDS_INC = antRouterSettings.getDouble("PREDS_INC");
+        else
+            PREDS_INC = DEFAULT_PREDS_INC;
 
-        PRED_INC = ant_router_settings.getDouble("PRED_INC");
-        PRED_DEC = ant_router_settings.getDouble("PRED_DEC");
-        PRED_DEC_RATE = ant_router_settings.getDouble("PRED_DEC_RATE");
+        if(antRouterSettings.contains("PREDS_DEC"))
+            PREDS_DEC = antRouterSettings.getDouble("PREDS_DEC");
+        else
+            PREDS_DEC = DEFAULT_PREDS_DEC;
+
+        if(antRouterSettings.contains("ANTS_PER_MSG"))
+            ANTS_PRE_MSG = antRouterSettings.getInt("ANTS_PER_MSG");
+        else
+            ANTS_PRE_MSG = DEFAULT_ANTS_PRE_MSG;
+
+        Init();
     }
 
-    /**
-     * Copy Constructor.
-     *
-     * @param r AntRouter Object.
-     */
-    private AntRouter(AntRouter r) {
+    public AntRouter(AntRouter r){
         super(r);
-
-        this.predictions = new HashMap<>();
-        this.known_host = new ArrayList<>();
-        this.predictions_update = new HashMap<>();
+        Init();
     }
 
-
-    /**
-     * update via timer interval.
-     */
-    @Override
-    public void update() {
-        super.update();
-        updateAntTable();
-        // nothing to do or can't transfer message.
-        if (isTransferring() || !canStartTransfer())
-            return;
-
-        // try to transfer message to final replicate.
-        if (exchangeDeliverableMessages() != null)
-            return;
-
-        // try find hosts / find route / transfer message by route
-        doPrivateMessage();
+    private void Init(){
+        predictions = new HashMap<>();
+        update_time = new HashMap<>();
     }
 
     @Override
-    public int receiveMessage(Message msg, DTNHost from) {
-
-        // If Ant Packet, update predictions
-        if (msg.getId().contains("Ant")) {
-
-            // Direct from is updated for PRED_INC
-            if (predictions.containsKey(from)) {
-                predictions.put(from, predictions.get(from) + PRED_INC);
-                predictions_update.put(from, SimClock.getTime());
-            } else {
-                predictions.put(from, PRED_INC);
-                predictions_update.put(from, SimClock.getTime());
-            }
-
-            // Predictions will decrease for hops
-            DTNHost host = msg.getFrom();
-            int HopCount = msg.getHopCount();
-            if (predictions.containsKey(host)) {
-                predictions.put(host, predictions.get(host) + PRED_INC * Math.pow(PRED_DEC_RATE, HopCount - 1));
-                predictions_update.put(host, SimClock.getTime());
-            } else {
-                predictions.put(host, PRED_INC * Math.pow(PRED_DEC_RATE, HopCount - 1));
-                predictions_update.put(host, SimClock.getTime());
-            }
-
-            // If ECHO, send Reply
-            if (msg.getId().contains("Echo")) {
-                Message return_msg = new Message(msg.getTo(), msg.getFrom(), "Ant Reply --" + msg.getFrom() + "-" + msg.getTo(), msg.getSize());
-                createNewMessage(return_msg);
-            }
-        }
-
-        return super.receiveMessage(msg, from);
-    }
-
-    /**
-     * While connection changed, send ant packats and update routing table.
-     *
-     * @param conn Connection Object
-     */
-    @Override
-    public void changedConnection(Connection conn) {
+    public void changedConnection(Connection conn){
         super.changedConnection(conn);
-
-        if (conn.isUp()) {
-            DTNHost other_host = conn.getOtherNode(getHost());
-            updateKnownHost(other_host);
-        }
     }
 
     @Override
-    public RoutingInfo getRoutingInfo() {
-        RoutingInfo top = super.getRoutingInfo();
-        RoutingInfo ret = new RoutingInfo(predictions.size() + " delivery prediction(s)");
+    public int receiveMessage(Message msg,DTNHost from){
+        int ret = super.receiveMessage(msg,from);
 
-        for (Map.Entry<DTNHost, Double> entry : predictions.entrySet()) {
-            DTNHost host = entry.getKey();
+        double preds = getPreds(from) + PREDS_INC * ((Number)(msg.getProperty("ants"))).intValue();
+        predictions.put(from,preds);
+        update_time.put(from,SimClock.getTime());
+
+        return ret;
+    }
+
+    @Override
+    public boolean createNewMessage(Message msg){
+        // add property to set ants to transfer this message.
+        if(msg.getProperty("ants")==null) {
+            msg.addProperty("ants", ANTS_PRE_MSG);
+        }
+        return super.createNewMessage(msg);
+    }
+
+    @Override
+    public void update(){
+        super.update();
+
+        // nothing to transfer or is transferring
+        if(!canStartTransfer()|| isTransferring())return;
+
+        // try messages that could be delivered to final recipient
+        if(exchangeDeliverableMessages() != null)return;
+
+        tryOtherMessage();
+
+    }
+
+    @Override
+    public MessageRouter replicate(){
+        return new AntRouter(this);
+    }
+
+    @Override
+    public RoutingInfo getRoutingInfo(){
+        RoutingInfo top = super.getRoutingInfo();
+        RoutingInfo ret = new RoutingInfo(predictions.size()+" delivery predictabilities");
+
+        for(Map.Entry<DTNHost,Double> entry : predictions.entrySet()){
+            DTNHost host  = entry.getKey();
+            updatePreds(host);
             double preds = entry.getValue();
 
-            ret.addMoreInfo(new RoutingInfo(String.format("%s : %.6f", host, preds)));
+            ret.addMoreInfo(new RoutingInfo(String.format("%s : %.6f",host,preds)));
         }
-
         top.addMoreInfo(ret);
         return top;
     }
 
     /**
-     * If passed UPDATE_INTERVAL, generate and send ANT_ECHO_PACKET
-     * And decrease predictions anytime.
+     * Get this router's delivery prediction about the host.
+     * @param host Target host
+     * @return a double value(prediction of the host).
      */
-    private void updateAntTable() {
-
-        double now_time = SimClock.getTime();
-        if ((now_time - last_update_time) / UPDATE_INTERVAL - 1 >= 0) {
-
-            for (DTNHost host : known_host) {
-                if (predictions_update.containsKey(host)) {
-                    double update_time = predictions_update.get(host);
-                    double pred = predictions.get(host);
-
-                    if (pred - PRED_DEC > 0)
-                        predictions.put(host, pred - PRED_DEC);
-                    else
-                        predictions.put(host, 0.0);
-
-                    if (now_time - update_time - UPDATE_INTERVAL > 0) {
-                        Message msg = new Message(getHost(), host, "Ant Echo --" + getHost() + "-" + host, 64);
-                        createNewMessage(msg);
-                        predictions_update.put(host, now_time);
-                    }
-                } else {
-                    Message msg = new Message(getHost(), host, "Ant Echo --" + getHost() + "-" + host, 64);
-                    createNewMessage(msg);
-                }
-            }
-
-            last_update_time = now_time;
+    private double getPreds(DTNHost host){
+        if(predictions.containsKey(host)){
+            updatePreds(host);
+            return predictions.get(host);
         }
-
+        return 1.0;
     }
 
     /**
-     * Get the prediction of the host in this router's predictions table
-     *
-     * @param host DTNHost Object
-     * @return double value as prediction
+     * update  predictions via time pass
+     * predictions is decreasing
+     * @param host Target DTNHost to update
      */
-    public double getPred(DTNHost host) {
-        if (predictions.containsKey(host)) return predictions.get(host);
-        return 0.0;
+    private void updatePreds(DTNHost host){
+        double now = SimClock.getTime();
+        int pass = (int)((now - update_time.get(host))/UPDATE_INTERVAL);
+        predictions.put(host,predictions.get(host) * Math.pow(PREDS_DEC,pass));
+        update_time.put(host,now);
     }
 
     /**
-     * Tries to send all other messages to all connected hosts ordered by
-     * their probability
-     *
-     * @return The return value of {@link #tryMessagesForConnected(List)}
+     * Tries to send all other messages to all connected hosts ordered by their delivery probability.
+     * @return value of {@link #tryMessagesForConnected(List)}
      */
-    private Tuple<Message, Connection> doPrivateMessage() {
-        List<Tuple<Message, Connection>> messages = new ArrayList<Tuple<Message, Connection>>();
+    private Tuple<Message,Connection> tryOtherMessage(){
+        List<Tuple<Message,Connection>> messages = new ArrayList<>();
 
-        Collection<Message> msg_collection = getMessageCollection();
+        Collection<Message> msgCollection = getMessageCollection();
 
-        for (Connection conn : getConnections()) {
+        /*
+        for(Connection conn:getConnections()){
             DTNHost other_host = conn.getOtherNode(getHost());
-            AntRouter other_router = (AntRouter) other_host.getRouter();
+            AntRouter other_router = (AntRouter)other_host.getRouter();
 
-            // skip host is transferring.
             if (other_router.isTransferring()) continue;
 
-            for (Message msg : msg_collection) {
+            for(Message msg:msgCollection) {
                 if (other_router.hasMessage(msg.getId())) continue;
-
-                if (getPred(msg.getTo()) > other_router.getPred(msg.getTo())) {
+                if (other_router.getPreds(msg.getTo()) > getPreds(msg.getTo())) {
                     messages.add(new Tuple<>(msg, conn));
                 }
             }
         }
-        if (messages.size() == 0) return null;
+        */
+
+        for(Message msg:msgCollection){
+            int ants = ((Number)msg.getProperty("ants")).intValue();
+            double sum_of_preds = 0;
+
+            // get sum of predictions
+            for(Connection conn:getConnections()){
+                AntRouter other_router = (AntRouter)conn.getOtherNode(getHost()).getRouter();
+                if(other_router.isTransferring())continue;
+                if(other_router.hasMessage(msg.getId()))continue;
+                if(other_router.getPreds(msg.getTo()) > getPreds(msg.getTo())){
+                    sum_of_preds += other_router.getPreds(msg.getTo());
+                }
+            }
+
+            // let ants go and separated via preds divided by sum_of_preds
+            for(Connection conn:getConnections()){
+                AntRouter other_router = (AntRouter)conn.getOtherNode(getHost()).getRouter();
+                msg.updateProperty("ants",(int)(ants * (other_router.getPreds(msg.getTo())/sum_of_preds)));
+                messages.add(new Tuple<>(msg,conn));
+            }
+        }
+        if(messages.size() == 0) return null;
 
         messages.sort(new TupleComparator());
-
-        // try to send messages
         return tryMessagesForConnected(messages);
     }
 
-
     /**
-     * Update known host for generating ant packet.
-     *
-     * @param host DTNHost
+     * a comparator of Tuple<Message,Connnection>
      */
-    private void updateKnownHost(DTNHost host) {
-        if (!known_host.contains(host)) known_host.add(host);
-    }
+    private class TupleComparator implements Comparator<Tuple<Message,Connection>>{
+        public int compare(Tuple<Message,Connection> t1,Tuple<Message,Connection> t2){
+            double p1 = ((AntRouter)t1.getValue().getOtherNode(getHost()).getRouter()).getPreds(t1.getKey().getTo());
+            double p2 = ((AntRouter)t2.getValue().getOtherNode(getHost()).getRouter()).getPreds(t2.getKey().getTo());
 
-    @Override
-    public AntRouter replicate() {
-        return new AntRouter(this);
-    }
-
-    /**
-     * Sort On_send_messages predictions. Bigger is higher.
-     */
-    private class TupleComparator implements Comparator
-            <Tuple<Message, Connection>> {
-        public int compare(Tuple<Message, Connection> tp1, Tuple<Message, Connection> tp2) {
-            double pred1 = ((AntRouter) tp1.getValue().getOtherNode(getHost()).getRouter()).getPred(tp1.getKey().getTo());
-            double pred2 = ((AntRouter) tp2.getValue().getOtherNode(getHost()).getRouter()).getPred(tp2.getKey().getTo());
-
-            if (pred2 - pred1 == 0) return 0;
-            else if (pred2 - pred1 < 0) return -1;
+            if(p2 - p1 == 0)
+                return compareByQueueMode(t1.getKey(),t2.getKey());
+            else if(p2-p1< 0)return -1;
             else return 1;
         }
     }
